653e08b17912ce5c4e11d074c395fa58279971a0
Fakhri Fitra <fitrafakhri49@gmail.com>
2026-01-21 16:08:32 +0700

    feat:add logout button to logout

--- frontend/app/admin/dashboard/page.tsx
+++ frontend/app/admin/dashboard/page.tsx
@@ -1,14 +1,22 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export default function Dashboard() {
   const [bookings, setBookings] = useState<any[]>([]);
+  const router = useRouter();
 
   useEffect(() => {
     const token = localStorage.getItem("access_token");
+
+    // guard: kalau belum login, langsung ke login
+    if (!token) {
+      router.push("/admin/login");
+      return;
+    }
 
     fetch(`${BASE_URL}/api/bookings`, {
       headers: {
@@ -17,11 +25,19 @@ export default function Dashboard() {
     })
       .then((res) => res.json())
       .then(setBookings);
-  }, []);
+  }, [router]);
+
+  const handleLogout = () => {
+    localStorage.removeItem("access_token");
+    router.push("/admin/login"); // arahkan ke halaman login
+  };
 
   return (
     <main>
-      <h1>Admin Dashboard</h1>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <h1>Admin Dashboard</h1>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
 
       <ul>
         {bookings.map((b) => (
