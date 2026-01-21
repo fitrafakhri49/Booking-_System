0c6cb757f1c26108afb76d726a6b292a0a8b7783
Fakhri Fitra Perdana <160821624+fitrafakhri49@users.noreply.github.com>
2026-01-21 15:12:49 +0700

    feat:connect_backend_to_frontend (#1)
    Working

---
+++ frontend/app/admin/dashboard/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+export default function Dashboard() {
+  const [bookings, setBookings] = useState<any[]>([]);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    fetch(`${BASE_URL}/api/bookings`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then(setBookings);
+  }, []);
+
+  return (
+    <main>
+      <h1>Admin Dashboard</h1>
+
+      <ul>
+        {bookings.map((b) => (
+          <li key={b.id}>
+            {b.name} | {new Date(b.start_time).toLocaleString()} -{" "}
+            {new Date(b.end_time).toLocaleString()}
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
